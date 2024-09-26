e52e6e2bf39d9ba55161a75a4cbbbec540cac0bf
PoczciwyPatryk <154216910+PoczciwyPatryk@users.noreply.github.com>
2024-09-26 15:03:10 +0200

    Add files via upload

---
+++ siubdup/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+vector<int> ulamki;
+
+
+int main()
+{
+    ifstream plik("dane_ulamki.txt");
+    for(int i=0, i<20; i++);{
+        cout<<ulamki[i]<<endl;
+    }
+    cout<<"Ulamki"<<endl;
+
+
+    return 0;
+}
